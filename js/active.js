faedb897f7e3c0344d2dc927d86d5fcc7d507505
kwarchalowski <karol@warchalowski.pl>
2021-08-22 15:35:48 +0200

    Active list item script

---
+++ js/active.js
@@ -0,0 +1,15 @@
+// active list item selector: 
+let list = document.querySelectorAll('.list');
+
+for (let i = 0; i < list.length; i++) {
+
+    list[i].onclick = function() {
+        let j = 0;
+
+        while (j < list.length) {
+            list[j++].className = 'list';
+        }
+
+        list[i].className = 'list active';
+    }
+}
